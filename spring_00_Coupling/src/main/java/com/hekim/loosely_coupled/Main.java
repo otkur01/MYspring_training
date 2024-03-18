525299df56248e1cf16641a24a0c9755f8d6ad5b
otkur <abdulhekem@gmail.com>
2024-03-18 06:11:17 +0300

    my first spring /coupled

---
+++ spring_00_Coupling/src/main/java/com/hekim/loosely_coupled/Main.java
@@ -0,0 +1,25 @@
+package com.hekim.loosely_coupled;
+
+import com.hekim.loosely_coupled.casefactory.Case;
+import com.hekim.loosely_coupled.casefactory.DellCase;
+import com.hekim.loosely_coupled.monitorfactory.AcerMonitor;
+import com.hekim.loosely_coupled.monitorfactory.Monitor;
+import com.hekim.loosely_coupled.motherboardfactory.AsusMotherboard;
+import com.hekim.loosely_coupled.motherboardfactory.Motherboard;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        Monitor theMonitor = new AcerMonitor("27inch Beast", "Acer", 27);
+        Case theCase = new DellCase("220B", "Dell", "240");
+        Motherboard theMotherboard = new AsusMotherboard("BJ-200", "Asus", 4, 6, "v2.44");
+
+        PC myPc = new PC(theCase,theMonitor,theMotherboard);
+
+        myPc.powerUp();
+
+
+    }
+}

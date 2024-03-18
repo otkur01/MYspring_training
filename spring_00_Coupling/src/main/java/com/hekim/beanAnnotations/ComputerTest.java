1e2d0b53980726933a8f49c48503a44acbbb6002
otkur <abdulhekem@gmail.com>
2024-03-18 14:16:18 +0300

    creating config modoules

--- spring_00_Coupling/src/main/java/com/hekim/beanAnnotations/ComputerTest.java
+++ spring_00_Coupling/src/main/java/com/hekim/beanAnnotations/ComputerTest.java
@@ -2,6 +2,8 @@ package com.hekim.beanAnnotations;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
 public class ComputerTest {
     public static void main(String[] args) {
 

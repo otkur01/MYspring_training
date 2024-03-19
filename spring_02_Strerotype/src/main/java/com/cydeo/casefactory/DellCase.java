a9c5041f04e409144c9df285a194b9e5cd93200e
otkur <abdulhekem@gmail.com>
2024-03-19 20:43:40 +0300

    using component anotaition

---
+++ spring_02_Strerotype/src/main/java/com/cydeo/casefactory/DellCase.java
@@ -0,0 +1,20 @@
+package com.cydeo.casefactory;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Getter
+@Setter
+@Component
+public class DellCase extends Case {
+
+    public DellCase(String model, String manufacturer, String powerSupply) {
+        super(model, manufacturer, powerSupply);
+    }
+
+    public void pressPowerButton() {
+
+        System.out.println("Power button pressed");
+    }
+}
---
+++ spring_02_Strerotype/src/main/java/com/cydeo/monitorfactory/Monitor.java
@@ -0,0 +1,17 @@
+package com.cydeo.monitorfactory;
+
+
+public abstract class Monitor {
+
+    private String model;
+    private String manufacturer;
+    private int size;
+
+    public Monitor(String model, String manufacturer, int size) {
+        this.model = model;
+        this.manufacturer = manufacturer;
+        this.size = size;
+    }
+
+    public abstract void drawPixelAt();
+}

da656eb71812332b61a12c659985507f3c18a854
otkur <abdulhekem@gmail.com>
2024-03-20 00:00:43 +0300

    wiring started

--- Wiring_03/src/main/java/com/cydeo/casefactory/Demensions.java
+++ Wiring_03/src/main/java/com/cydeo/casefactory/Demensions.java
@@ -1,4 +1,20 @@
 package com.cydeo.casefactory;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+
 public class Demensions {
+    private int width;
+    private int height;
+    private int depth;
+
+    public void pressPowerButton(){
+        System.out.println("power button pressed");
+    }
+
 }
